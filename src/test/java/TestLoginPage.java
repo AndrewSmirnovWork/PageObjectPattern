fa5e22966ebf534142a28d6e42b565924a36ccb6
AndrewSmirnovWork <andrew.smirnov.work@gmail.com>
2021-01-18 16:30:16 +0300

    first commit/can't go home page

--- src/test/java/TestLoginPage.java
+++ src/test/java/TestLoginPage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -19,19 +20,30 @@ public class TestLoginPage {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Luck\\Desktop\\chromedriver.exe");
         driver = new ChromeDriver();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://igorakintev.ru/");
+
     }
 
     @Test
+    public void testLogin() {
+        driver.get("https://igorakintev.ru/");
+        loginPage.typeUsername("selenium")
+                .typePassword("super_password")
+                .submitLogin();
+
+        Assert.assertEquals(driver.getTitle(), "Администрирование сайта | Панель управления");
+    }
+
+    /*@Test
     public void test_Home_Page_Appear_Correct(){
         //Create Login Page object
         loginPage = new LoginPage(driver);
         //login to application
         loginPage.loginAs("selenium", "super_password");
         // go the next page
+        String title = driver.getCurrentUrl();
 
         //Verify home page
         System.out.println(homePage.getHomePageDashboardName());
         //Assert.assertTrue(homePage.getHomePageDashboardName().toLowerCase().contains("admin"));
-    }
+    }*/
 }
